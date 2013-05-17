297a96c246f97a232aaa413dfcf8ad0420bba69b
Shane Grimm <shane_grimm@hotmail.com>
2013-05-17 12:35:02 -0400

    First commit
    Testing, using this repo to share ACL Scripts as .BAT files.

---
+++ Execute_Wait.bat
@@ -0,0 +1,23 @@
+COMMENT *** This requires AN10 or higher
+COMMENT *** Also consider WAITFOR to send or wait for a signal from another computer
+
+SET SESSION Test Timeout
+
+COMMENT *** Pause for 10 seconds or until key is pressed
+
+DISP TIME()
+EXECUTE "TIMEOUT /t 10"
+DISP TIME()
+
+COMMENT *** Pause for 30 seconds, no option to press key to bypass
+COMMENT *** User still has option to press Ctrl+C to quit
+
+DISP TIME()
+EXECUTE "TIMEOUT /t 10 /nobreak"
+DISP TIME()
+
+COMMENT *** Pause until a key is pressed (Don't use with AX)
+
+EXECUTE "TIMEOUT /t -1"
+
+SET SESSION
---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1>Shane's ACL Script repository</h1>
+
+My first Github repository for storing and sharing ACL Utility scripts. 
+
+Comments are welcome, please e-mail me using my full name (with an underscore between) @acl.com. 
