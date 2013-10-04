68e83475a6a0d6027035b5aa98a8c32dba47b5d0
Shane Grimm <shane_grimm@hotmail.com>
2013-10-04 15:52:42 -0400

    Comments and create stub file

--- Import CSV/ImportCSV.bat
+++ Import CSV/ImportCSV.bat
@@ -1,3 +1,6 @@
+SET SESSION "ImportCSV"
+SET SAFETY OFF
+
 COMMENT
   ***********************************************************
   An attempt to auto define a delimited file using a script.
@@ -11,10 +14,17 @@ END
 
 COMMENT 
   ***********************************************************
-  Create a new ACL Table with a name of v_Table and a length
-  of v_Recordlength
+  Create a new ACL Table with a name of v_Table and a length of 
+  v_Recordlength by creating .FMT and opening CSV with the new FMT.
+  If not using AN10.5 same can be done by using t_stub and defining
+  v_line as a computed field with BLANKS(%v_Recordlength%) and
+  then extrating v_line to a new table then relinking to the csv
   ***********************************************************
 END
+
+COMMENT *** Create stub file since ACL requires a table to be open
+DIRECTORY *.acl TO t_stub
+OPEN t_stub
 
 v_Table = "Employees"
 v_Recordlength = "250" 
@@ -25,6 +35,7 @@ LIST UNFORMATTED "FILE_NAME '%v_Table%.CSV'"                           TO %v_Tab
 LIST UNFORMATTED " "                                                   TO %v_Table%.FMT IF RECNO() = 0 EOF APPEND
 LIST UNFORMATTED "RECORD_LENGTH %v_Recordlength% SKIP 0 ASCII CRLF"    TO %v_Table%.FMT IF RECNO() = 0 EOF APPEND
 LIST UNFORMATTED "v_line         ASCII       1  %v_Recordlength%    "  TO %v_Table%.FMT IF RECNO() = 0 EOF APPEND
+
 
 IMPORT FORMAT "%v_Table%.FMT" TO %v_Table%
 OPEN "%v_Table%.csv" FORMAT "%v_Table%.FMT"
@@ -46,3 +57,14 @@ COMMENT *** Extract all of the fields to new table... maybe not required?
 EXTRACT FIELDS %v_fieldlist% TO "Test"
 OPEN Test
 
+COMMENT 
+  ***********************************************************
+  Cleanup all temporary files and variables
+  ***********************************************************
+END
+
+DELETE FORMAT t_stub OK
+DELETE t_stub.FIL OK
+
+SET SAFETY ON
+SET SESSION
